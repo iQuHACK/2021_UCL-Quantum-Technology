278769aa4ec1149b880e12a28e02d4d70502a588
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2021-01-30 21:01:59 +0000

    Solution 14

---
+++ boxes_hybrid.py
@@ -0,0 +1,30 @@
+# Copyright 2020 D-Wave Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from dwave.system import LeapHybridSampler
+sampler = LeapHybridSampler()
+
+GAMMA = 25
+
+Q = {('X1','X1'): 17 - 3*GAMMA,
+     ('X2','X2'): 21 - 3*GAMMA, 
+     ('X3','X3'): 19 - 3*GAMMA, 
+     ('X1','X2'): 2*GAMMA,
+     ('X1','X3'): 2*GAMMA, 
+     ('X2','X3'): 2*GAMMA}
+
+results = sampler.sample_qubo(Q)
+
+# print the results
+for sample, energy in results.data(['sample', 'energy']):
+    print(sample, energy)
---
+++ exactly_one_hybrid.py
@@ -0,0 +1,28 @@
+# Copyright 2020 D-Wave Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from dwave.system import LeapHybridSampler
+sampler = LeapHybridSampler()
+
+Q = {('B','B'): -1,
+     ('G','G'): -1, 
+     ('R','R'): -1, 
+     ('B','R'): 2,
+     ('B','G'): 2, 
+     ('G','R'): 2}
+
+results = sampler.sample_qubo(Q)
+
+# print the results
+for sample, energy in results.data(['sample', 'energy']):
+    print(sample, energy)
