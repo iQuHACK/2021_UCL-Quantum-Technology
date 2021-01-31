2dbb1c60f3a6955f9ab8e479f9f60d4a0b487eca
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2021-01-31 16:40:19 +0000

    Holiday planner exact solver

---
+++ holiday_planner_v2.py
@@ -0,0 +1,61 @@
+
+from networkx import nx
+import dwave_networkx as dnx
+from dwave.system import LeapHybridSampler
+sampler = LeapHybridSampler()
+import dimod
+
+#lagrange = 10
+
+#num_colors = 1  #only one colour for travelling salesman
+#colors = range(num_colors)
+#dqm = DiscreteQuadraticModel()
+G = nx.Graph()
+G.add_weighted_edges_from({ (0, 1, 1), 
+                            (0, 2, 50),
+                            (0, 3, 51), 
+                            (1, 2, 1),
+                            (1, 3, 2),
+                            (2, 3, 1)
+                                    })
+#G.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (0, 6)])
+#n_edges = len(G.edges)
+#print(G.nodes)
+#print(G.edges)
+#for p in G.nodes:
+#    dqm.add_variable(num_colors, label=p)
+#for p in G.nodes:
+#    dqm.set_linear(p, colors)           
+#for p0, p1 in G.edges:
+#dqm.set_quadratic(p0, p1, {(c, c): G.weights})
+#dqm.set_quadratic(0, 1, {(0, 0): 1})
+#dqm.set_quadratic(0, 2, {(0, 0): 50})
+#dqm.set_quadratic(0, 3, {(0, 0): 51})
+#dqm.set_quadratic(1, 2, {(0, 0): 1})
+#dqm.set_quadratic(1, 3, {(0, 0): 2})
+#dqm.set_quadratic(2, 3, {(0, 0): 1})
+
+#sampler = LeapHybridDQMSampler()
+#sampleset = sampler.sample_dqm(dqm)
+#print(sampleset)
+#sample = sampleset.first.sample
+#energy = sampleset.first.energy
+#valid = True
+#for edge in G.edges:
+#    i, j = edge
+#    if sample[i] == sample[j]:
+#        valid = False
+#        break
+#print("Solution: ", sample)
+#print("Solution energy: ", energy)
+#print("Solution validity: ", valid)
+
+#CASE = "EXACT"
+
+#dqm = DiscreteQuadraticModel()
+
+#print(dnx.traveling_salesperson(G, LeapHybridDQMSampler(), start=0))
+
+print(dnx.traveling_salesperson(G, dimod.ExactSolver(), start=0))
+
+
